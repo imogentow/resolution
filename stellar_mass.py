15487abdb0af6a966014b43ef0f8b6659068731a
Imogen Towler <imogen.towler@gmail.com>
2023-12-11 14:31:00 +0000

    Flipped axes.

--- stellar_mass.py
+++ stellar_mass.py
@@ -61,12 +61,13 @@ def calc_and_plot(sim_series, ax, snap_id=36):
         sort_masses = np.flip(np.sort(masses))
         sort_masses = sort_masses[np.where(sort_masses > 0)[0]]
         cumulative_mass = np.cumsum(sort_masses)
-
-        ax.loglog(cumulative_mass/tot_mass, color=cm[i], label=sim)
+        N_halos = len(sort_masses)
+        halo_range = np.arange(1, N_halos+1, 1)
+        ax.loglog(cumulative_mass/tot_mass, halo_range, color=cm[i], label=sim)
     ax.legend()
 
 
-def cumulative_masses(*sims, snap_id=36):
+def cumulative_masses(snap_id=36):
     fig, ax = plt.subplots(nrows=3, ncols=1, 
                              figsize=(3,6),
                              sharex=True, 
@@ -75,13 +76,12 @@ def cumulative_masses(*sims, snap_id=36):
     calc_and_plot("R", ax[0], snap_id=snap_id)
     calc_and_plot("K", ax[1], snap_id=snap_id)
     calc_and_plot("M", ax[2], snap_id=snap_id)
-    ax[2].set_xlabel("$N_{\\rm{halos}}$")
-    ax[1].set_ylabel("$M_{\\rm{cumulative}} / M_{\\rm{200c},\star}$")
+    ax[1].set_ylabel("$N_{\\rm{halos}}$")
+    ax[2].set_xlabel("$M_{\\rm{cumulative}} / M_{\\rm{200c},\star}$")
     plt.legend()
     plt.subplots_adjust(left=0.2, right=0.99)
-    filename = "cumulative_stellar_mass.png"
+    filename = "plots/cumulative_stellar_mass.png"
     plt.savefig(filename, dpi=300)
     plt.show()
 
-
-cumulative_masses("R1", "R2", "R3", "R4")
+cumulative_masses()
